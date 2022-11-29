51bee21e65a3b97b4cc5d88459cef54221119eac
“reinhardnzomo <nzomo.mr@gmail.com>
2022-11-29 11:58:57 -0800

    correcting pycodestyle 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -19,10 +19,10 @@ last_digit = int(repr(number)[-1])
 
 if (number < 0):
     last_digit *= -1
-print('Last digit of {} is {} and is '.format(number, last_digit), end ='')
+print('Last digit of {} is {} and is '.format(number, last_digit), end='')
 if (last_digit > 5):
     print('greater than 5')
-elif (last_digit < 6):
-    print('less than 6 and not 0')
-else:
+elif (last_digit == 0):
     print('0')
+else:
+    print('less than 6 and not 0')
