684f58db8639d7107b666662a990e09bad219154
“reinhardnzomo <nzomo.mr@gmail.com>
2022-12-01 03:41:50 -0800

    correcting import sttmnt

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-add = __import__('add_0').add
-a, b = 1, 2
-print('{} + {} = {}'.format(a, b, add(a, b)))
+if __name__ == '__main__':
+    from add_0 import add
+    add = __import__('add_0').add
+    a, b = 1, 2
+    print('{} + {} = {}'.format(a, b, add(a, b)))
