5220fb7b38608704259a8b8ee4f750628f68d83a
“reinhardnzomo <nzomo.mr@gmail.com>
2022-12-01 09:38:11 -0800

    my calc pycode

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
     if (num_args != 3):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
-    if (sys.argv[2] != "+" and sys.argv[2] != "-" and sys.argv[2] != "*" and sys.argv[2] != "/"):
+
+    ops = sys.argv[2]
+    if (ops != "+" and ops != "-" and ops != "*" and ops != "/"):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
@@ -16,11 +17,11 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if (sys.argv[2] == "+"):
+    if (ops == "+"):
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif (sys.argv[2] == "-"):
+    elif (ops == "-"):
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif (sys.argv[2] == "*"):
+    elif (ops == "*"):
         print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
         print("{} / {} = {}".format(a, b, div(a, b)))
