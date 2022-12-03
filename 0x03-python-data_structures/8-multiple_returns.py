24dca9f9e921d0daa6f619d12bb0f1993aef7071
“reinhardnzomo <nzomo.mr@gmail.com>
2022-12-03 02:33:16 -0800

    correcting 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    s_tuple = ()
-    if len(sentence) < 0:
-        s_tuple = (len(sentence), None)
-        return (s_tuple)
-
-    s_tuple = (len(sentence), sentence[0])
-    return (s_tuple)
-
-
-'''
     if sentence == "":
         return (0, None)
     return (len(sentence), sentence[0])
-'''
