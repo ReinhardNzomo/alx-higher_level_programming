5fbca53546ec72c9225b74349013a761dfc6f5a7
“reinhardnzomo <nzomo.mr@gmail.com>
2022-12-07 04:06:08 -0800

    1-search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,17 @@
 #!/usr/bi/python3
-def search_replace(my_list, search, replace):
+'''def search_replace(my_list, search, replace):
     if not my_list:
         return None
     copy = []
     copy = [val if val != search else replace for val in my_list]
-    return (copy)
+    return (copy)'''
+
+
+def search_replace(my_list, search, replace):
+    copy = []
+    for a in range(len(my_list)):
+        if my_list[a] != search:
+            copy.append(my_list[a])
+        else:
+            copy.append(replace)
+    return copy
