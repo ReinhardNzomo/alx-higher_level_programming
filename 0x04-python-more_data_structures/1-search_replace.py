167e0b75c3a19bcbec94f06dd13a794da2eb656d
“reinhardnzomo <nzomo.mr@gmail.com>
2022-12-07 04:25:07 -0800

    extra

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,3 +2,4 @@
 def search_replace(my_list, search, replace):
     copy = list(map(lambda x: replace if x == search else x, my_list))
     return (copy)
+    '''return [val if val != search else replace for val in my_list]'''
