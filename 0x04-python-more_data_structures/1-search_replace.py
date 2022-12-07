c9113b77398424061b704afc73ead83514948d01
“reinhardnzomo <nzomo.mr@gmail.com>
2022-12-07 04:01:22 -0800

    rm 1-s

--- 0x04-python-more_data_structures/1-search_replace.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bi/python3
-def search_replace(my_list, search, replace):
-    if not my_list:
-        return None
-    copy = [val if val != search else replace for val in my_list]
-    return (copy)
