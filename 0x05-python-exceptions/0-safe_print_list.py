f0ed4e9d6434a586d9684ed5e6f6066a070f7858
“reinhardnzomo <nzomo.mr@gmail.com>
2022-12-19 12:24:22 +0300

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,5 +8,5 @@ def safe_print_list(my_list=[], x=0):
                 counter += 1
         print('')
         return counter
-    except SyntaxError:
+    except IndexError:
         print("Error Syntax")
