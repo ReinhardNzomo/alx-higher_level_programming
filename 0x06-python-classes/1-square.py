257aafd5fcfcbcbd4e1007d6d2a05927ff4f7d84
“reinhardnzomo <nzomo.mr@gmail.com>
2022-12-20 15:07:36 +0300

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-"""Write a class Square that defines a square by: (based on 0-square.py)"""
-
-
 class Square:
     def __init__(self, size):
-        """Initialize class square
-
-        Args:
-            size (int): size of square
-        """
         self.__size = size
