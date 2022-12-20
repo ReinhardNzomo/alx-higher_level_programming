c1c91d4fe1ddeba9d239bab22f1b6126989fb52e
“reinhardnzomo <nzomo.mr@gmail.com>
2022-12-21 00:43:06 +0300

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""Write a class Square that defines a square by size"""
+
+
 class Square():
+    """class rep a square"""
+
     def __init__(self, size=0):
+        """Initialize class square
+        Args:
+            size (int): size of square
+        """
         if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif (size < 0):
@@ -8,4 +17,5 @@ class Square():
         self.__size = size
 
     def area(self):
+        """return area of square"""
         return (self.__size ** 2)
